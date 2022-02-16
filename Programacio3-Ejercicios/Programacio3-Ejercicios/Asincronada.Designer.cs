3251f52bfd87ea4af8439c869c84d04ae2e4a217
ErickaAmador <98490079+ErickaAmador@users.noreply.github.com>
2022-02-15 22:40:21 -0600

    Asincronadas
    8 de febrero

---
+++ Programacio3-Ejercicios/Programacio3-Ejercicios/Asincronada.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Programacio3_Ejercicios
+{
+    partial class Asincronada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.HornearButton = new System.Windows.Forms.Button();
+            this.OtrasTareasButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // HornearButton
+            // 
+            this.HornearButton.Location = new System.Drawing.Point(160, 80);
+            this.HornearButton.Name = "HornearButton";
+            this.HornearButton.Size = new System.Drawing.Size(135, 23);
+            this.HornearButton.TabIndex = 2;
+            this.HornearButton.Text = "Hornear Pizza";
+            this.HornearButton.UseVisualStyleBackColor = true;
+            this.HornearButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OtrasTareasButton
+            // 
+            this.OtrasTareasButton.Location = new System.Drawing.Point(384, 80);
+            this.OtrasTareasButton.Name = "OtrasTareasButton";
+            this.OtrasTareasButton.Size = new System.Drawing.Size(75, 23);
+            this.OtrasTareasButton.TabIndex = 3;
+            this.OtrasTareasButton.Text = "Otras Tareas";
+            this.OtrasTareasButton.UseVisualStyleBackColor = true;
+            this.OtrasTareasButton.Click += new System.EventHandler(this.OtrasTareasButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 277);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Sumar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(160, 343);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Asincronada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OtrasTareasButton);
+            this.Controls.Add(this.HornearButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Asincronada";
+            this.Text = "Asincronada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button HornearButton;
+        private System.Windows.Forms.Button OtrasTareasButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ Programacio3-Ejercicios/Programacio3-Ejercicios/Asincronada.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacio3_Ejercicios
+{
+    public partial class Asincronada : Form
+    {
+        public Asincronada()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //HornearPizza();
+            await HornearPizzaAsync();
+            label1.Text = "La pizza esta lista";
+        }
+
+        private void HornearPizza()
+        {
+            Thread.Sleep(10000);
+        }
+
+        private Task HornearPizzaAsync()
+        {
+            return Task.Delay(10000);
+        }
+
+        private void OtrasTareasButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Realizando otras tareas","",MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            decimal valor1 = Convert.ToDecimal(textBox1.Text);
+            decimal valor2 = Convert.ToDecimal(textBox2.Text);
+
+            MessageBox.Show("La suma es: " + await SumarAsync(valor1, valor2));
+        }
+
+        private decimal Sumar(decimal valor1, decimal valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        private async Task<decimal> SumarAsync(decimal valor1, decimal valor2)
+        {
+            var resultado = await Task.Run(() =>
+                 {
+                     return valor1 + valor2;
+                 });
+            return resultado;
+        }
+    }
+}
